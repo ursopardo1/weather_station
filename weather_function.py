5be35c1c227c99290991a8d138e0c9086208c8b2
ursopardo1 <levismurici@gmail.com>
2021-02-16 19:33:52 -0300

    upload 2

--- weather_function.py
+++ weather_function.py
@@ -1,5 +1,11 @@
 def unpack_data(inc_data):
     dep_data = inc_data.split()
-    dec_data = {dep_data[0]: float(dep_data[1])}
+    dep_data[1] = int(dep_data[1])
+    dec_data = {dep_data[0]: dep_data[1]}
     return dec_data
 
+def media_data(dict0, dict1, dict2):
+    media_temp = ((dict0.get("T:")) + (dict1.get("T:")) + (dict2.get("T:")))/3
+    media_umi = ((dict0.get("L:")) + (dict1.get("L:")) + (dict2.get("L:")))/3
+    media_lum = ((dict0.get("U:")) + (dict1.get("U:")) + (dict2.get("U:")))/3
+    return media_lum, media_temp, media_umi
