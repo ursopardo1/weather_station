2f9afe117914b6b0997c6de0b8ddc126704bb1a8
ursopardo1 <levismurici@gmail.com>
2021-03-01 09:51:45 -0300

    Atualização - Código de loggin weather pronto;             - É preciso verificar sincronia;

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ mcu_connection = 'COM8'  # ('/dev/ttyUSB1')
 serial = serial.Serial(mcu_connection, 9600)
 serial.flushInput()
 
-avg_dict = {'Temperatura média: ': None, 'Umidade média: ': None}
+avg_dict = {'Temperatura média: ': None, 'Umidade média: ': None, 'Luminosidade: ': None}
 temp_data = []
 umi_data = []
+lumi_data = []
 
 while True:
     try:
@@ -27,22 +28,29 @@ while True:
 
                 decoded_data = decode_format(serial.readline())
                 working_data1 = unpack_data(decoded_data)
-                temp_data.append(working_data1.get('Temperatura:'))
+                temp_data.append(working_data1.get('Temperatura:'))  # Adiciona dado a lista para cálculo de média
                 sleep(2)
                 decoded_data = decode_format(serial.readline())
                 working_data2 = unpack_data(decoded_data)
-                umi_data.append(working_data2.get('Umidade:'))
-                print(temp_data)
-                print(umi_data)
+                umi_data.append(working_data2.get('Umidade:'))  # Adiciona dado a lista para cálculo de média
+                sleep(2)
+                decoded_data = decode_format(serial.readline())
+                working_data3 = unpack_data(decoded_data)
+                lumi_data.append(working_data3.get('Luminosidade:'))  # Adiciona dado a lista para cálculo de média
+                # print(temp_data)
+                # print(umi_data)
+                sleep(360)  # Espera 6 minutos, em um range de 10 amostras, para cálculo da média
 
             print(f"Média Temperatura: {avg_list(temp_data)}")
             print(f"Média Umidade: {avg_list(umi_data)}")
+            print(f"Média Luminosidade: {avg_list(lumi_data)}")
             avg_dict['Temperatura média: '] = avg_list(temp_data)
             avg_dict['Umidade média: '] = avg_list(umi_data)
+            avg_dict['Luminosidade: '] = avg_list(lumi_data)
             print(avg_dict)
         except:
             continue
-        with open(str(today)+'_data', "a") as f:
+        with open(str(today)+'_data', "a") as f:  # Escreve a média de 10 amostras em 1 hora, com data e hora
             # f.write(data_dict)
             # f.write('\n')
             writer = csv.writer(f, delimiter=",")
---
+++ weather_training.py
@@ -0,0 +1,31 @@
+from weather_function import unpack_data # Função de unpack string -> dict
+import serial
+from time import sleep
+import time
+import csv
+
+
+# serial = serial.Serial('/dev/ttyUSB1')
+# serial.flushInput()
+
+data_dict = {}
+data_media = {}
+
+while True:
+    try:
+        data_input = input(f'Insira um dado aleatório:')
+        try:
+            for num in range(1, 3):
+                decoded_data = data_input.decode("utf-8")
+                out_data = unpack_data(decoded_data)
+                data_dict.update(out_data)
+                sleep(2)
+                print(data_dict)
+        except:
+            continue
+        with open("w_data", "a") as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow([time.time(), data_dict])
+    except:
+        print("Script interrompido")
+        break
